c18b44d6214183deb26ab909231ccec29315b79f
Dinesh_DC <dineshshinde81563@gmail.com>
2025-04-13 23:13:51 +0530

    Exploring the materila ui

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,24 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import Button from "@mui/material/Button";
 import { red } from "@mui/material/colors";
+import { AppBar, Box, Typography } from "@mui/material";
 function App() {
-  const accent = red[500];
   return (
     <>
-      <Button variant="text" color="accent">
-        Click me
-      </Button>
+      <AppBar color="inherit" sx={{ padding: "1rem" }} >
+        <Typography>Hello!!</Typography>
+
+        <Box
+          component={"img"}
+          src="https://randomwordgenerator.com/img/picture-generator/51e6dc41434faa0df7c5d57bc32f3e7b1d3ac3e455517349762f72d794_640.jpg"
+          sx={{
+            height: "80px",
+            width: "80px",
+            borderRadius: "50%",
+            objectFit: "cover",
+          }}
+        />
+      </AppBar>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createTheme, ThemeProvider } from '@mui/material'
+
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark', // or 'dark'
+    primary: {
+      main: '#1976d2',
+    },
+  },
+});
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
-  </StrictMode>,
+  </ThemeProvider>,
 )
